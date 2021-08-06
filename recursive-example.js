2229aee52dd84083dac01869b585cf55c7fe8248
nishchhal <nishchhal@amniltech.com>
2021-08-06 11:24:47 +0545

    adds recursive function example

---
+++ recursive-example.js
@@ -0,0 +1,20 @@
+// Recursive function definitions has 2 clauses
+// It should call itself
+// There should be a stopping condition for recursion 
+
+const flatten = (input = [], result = []) => {
+    for(let item of input) {
+        if(typeof item == 'object') {
+            flatten(item, result);
+        } else {            
+            result.push(item)
+        }
+    }
+}
+
+let mixedArray = [100, 5, 'a', [3, 1, 5], 7, 9, [3, ['a', 'b']]]
+
+let result = []
+flatten(mixedArray, result);
+
+console.log(result);
